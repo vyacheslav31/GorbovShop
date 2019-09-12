a836dfbd5b44c1aefb46ebc7150a07fbf53c65a6
Vyacheslav <vyacheslav93@gmail.com>
2019-09-12 15:31:22 -0400

    modified theme

--- app/src/main/java/ca/vgorbov/gorbovshop/MenuActivity.java
+++ app/src/main/java/ca/vgorbov/gorbovshop/MenuActivity.java
@@ -1,13 +1,18 @@
 package ca.vgorbov.gorbovshop;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MenuActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
     }
 }
