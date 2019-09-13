8c8b8334bffeb0cc28518a2896ca04f0f23ab459
Vyacheslav <vyacheslav93@gmail.com>
2019-09-13 09:52:25 -0400

    1. Added Card View to cards. 2. Modified each individual card to reflected the different items on the menu. 3. Partially implemented 'add to cart' button

--- app/src/main/java/ca/vgorbov/gorbovshop/MenuActivity.java
+++ app/src/main/java/ca/vgorbov/gorbovshop/MenuActivity.java
@@ -3,16 +3,35 @@ package ca.vgorbov.gorbovshop;
 import android.app.ActionBar;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MenuActivity extends AppCompatActivity {
 
+    Button addToCartButton;
+    Button removeFromCartButton;
+    TextView qtyTextView;
+    TextView subtotalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+    }
+
+    public void addToCart(View view) {
+        ViewGroup layout = (ViewGroup) view.getParent();
+        TextView price = (TextView) layout.getChildAt(5);
+    }
+
+    public void removeFromCart(View view) {
     }
 }
